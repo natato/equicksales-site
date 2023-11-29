c1e67409ebfa9837c6a2bd8e7e00a095ea469216
supportcentre <supportcentre@ashesi.edu.gh>
2023-11-29 11:29:51 +0000

    Equicksales Site Commit
    Equicksales Site Commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<meta name="keywords" content="Online Marketing, Equicksales">
+		<meta name="description" content="Equicksales Consulting Ltd. is a software development company">
+		<meta name="author" content="Nathanael Asaam">
+        <title>Equicksales Consulting Ltd. </title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Custom Google font-->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body class="d-flex flex-column h-100">
+        <main class="flex-shrink-0">
+            <!-- Navigation-->
+            <nav class="navbar navbar-expand-lg navbar-light bg-white py-3">
+                <div class="container px-5">
+					<img src="assets/image/commerce.png"><br>
+					 <div class="fw-bolder text-primary" style="margin:5px;padding:5px;">Equicksales Consulting Ltd.</div>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+                            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="services.php">Services</a></li>
+                            <li class="nav-item"><a class="nav-link" href="products.php">Products</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- Header-->
+            <header>
+                <div class="container px-5 pb-5">
+                    <div class="row gx-5" style="width:100%; margin:0 auto;"> 
+                        <div class="col-xl-12">
+                            <!-- Header text content-->
+                            <div class="text-center text-xxl-start" >
+                                <div class="fs-3 fw-light text-muted">We can help your business with</div>
+                                <h1 class="display-3 fw-bolder mb-5"><span class="text-gradient d-inline">Online Marketing and Software Solutions</span></h1>
+                                <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
+                                    <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="products.php">Products</a>
+                                    <a class="btn btn-info btn-lg px-5 py-3 fs-6 fw-bolder" href="services.php">Services</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </header>
+            <!-- About Section-->
+            <section>
+                <div class="container px-5">
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-xxl-8">
+                            <div class="text-center my-5">
+                                <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Equicksales</span></h2>
+                                <p class="lead fw-light mb-4">Equicksales Consulting Ltd. is an IT consulting firm which specializes in software development consulting. We started in 2015</p>
+                                <p class="text-muted">Our mission is to help companies embark on software development projects given constrained timeline and budget.</p>
+                                <p class="text-muted">We have a vision to become one of the leading software development firms in Africa.</p>
+								<div class="d-flex justify-content-center fs-2 gap-4">
+                                    <a class="text-gradient" href="#!"><i class="bi bi-twitter"></i></a>
+                                    <a class="text-gradient" href="#!"><i class="bi bi-facebook"></i></a>
+									<a class="text-gradient" href="#!"><i class="bi bi-linkedin"></i></a>
+                                    <a class="text-gradient" href="#!"><i class="bi bi-github"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <!-- Footer-->
+        <footer class="bg-white py-4 mt-auto">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto"><div class="small m-0">Copyright &copy; Equicksales Consulting Ltd. <?php echo DATE("Y");?></div></div>
+                    <div class="col-auto">
+                        <a class="small" href="#!">Privacy</a>
+                        <span class="mx-1">&middot;</span>
+                        <a class="small" href="#!">Terms</a>
+                        <span class="mx-1">&middot;</span>
+                        <a class="small" href="#!">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+       <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>-->
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ products.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<meta name="keywords" content="Equicksales, Emedics, Eshop">
+		<meta name="description" content="The products of Equicksales Consulting Ltd. include an online marketing platform, an expert medical consultation system and a shop management system ">
+		<meta name="author" content="Nathanael Asaam">
+        <title>Equicksales Consulting Ltd.</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Custom Google font-->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body class="d-flex flex-column h-100 bg-light">
+        <main class="flex-shrink-0">
+            <!-- Navigation-->
+            <nav class="navbar navbar-expand-lg navbar-light bg-white py-3">
+                <div class="container px-5">
+                    <img src="assets/image/commerce.png"><br>
+					 <div class="fw-bolder text-primary" style="margin:5px;padding:5px;">Equicksales Consulting Ltd.</div>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+                            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="services.php">Services</a></li>
+                            <li class="nav-item"><a class="nav-link" href="products.php">Products</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- Page Content-->
+            <div class="container px-5 my-5">
+                <div class="text-center mb-5">
+                    <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Products</span></h1>
+                </div>
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-lg-11 col-xl-9 col-xxl-8">
+                        <!-- Experience Section-->
+                        <section>
+                            <div class="d-flex align-items-center justify-content-between mb-4">
+                                <h2 class="text-primary fw-bolder mb-0">Equicksales Marketing Platform</h2>
+                              
+                            </div>
+
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                       
+                                        <div class="col-lg-12">
+										<div>
+											Our Equicksales Marketing Platform is a platform for marketing and sales of products and services. As a business you can advertise you product or service on this platform to reach your customers. As a customer you can find products or services you want to patronize.
+										</div>
+
+										</div>
+                                    </div>
+									<div style="margin:10px;">
+										<a class="btn btn-info btn-lg px-5 py-3 fs-6 fw-bolder" href="http://localhost/new/equicksales/index.php/site/registration">Try for free</a>
+									</div>
+                                </div>
+                            </div>
+                        </section>
+                        <!-- Education Section-->
+                        <section>
+                            <h2 class="text-secondary fw-bolder mb-4">Emedics: An Expert Medical Consultation System</h2>
+                        
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                      
+                                        <div class="col-lg-12">
+											<div>Emedics is an Expert Medical Consultation System. It can assist Medical Doctors in Medical Consultation by suggesting the Doctor's diagnosis, medical test, and drug prescription associated with medical information captured during medical consultation such as symptoms and reactions of Patients. It can also be used as a regular hospital management system. You can use it to manage the day to day activities of a hospital. It has features for managing patients and dispensary records and medical consultation.
+											</div>
+										</div>
+                                    </div>
+									<div style="margin:10px;">
+										<a class="btn btn-primary btn-lg px-5 py-3 fs-6 fw-bolder" href="http://localhost/emedics">Try for free</a>
+									</div>
+                                </div>
+                            </div>
+                           
+                        </section>
+                        <!-- Divider-->
+                        <div class="pb-5"></div>
+                        <!-- Skills Section-->
+                        <section>
+                            <!-- Skillset Card-->
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <!-- Professional skills list-->
+                                    <div class="mb-5">
+                                        <div class="d-flex align-items-center mb-4">
+                                            <!--<div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-tools"></i></div>-->
+                                            <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Eshop: A Shop Management System</span></h3>
+                                        </div>
+										  <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                      
+                                        <div class="col-lg-12"><div>Eshop is a shop management system for managing shops and supermarkets. You can use it to manage inventory, sales, and administration of your shop</div></div>
+                                    </div>
+                                </div>
+                            </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <!-- Footer-->
+        <footer class="bg-white py-4 mt-auto">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto"><div class="small m-0">Copyright &copy; Equicksales Consulting Ltd. <?php echo DATE("Y");?></div></div>
+                    <div class="col-auto">
+                        <a class="small" href="#!">Privacy</a>
+                        <span class="mx-1">&middot;</span>
+                        <a class="small" href="#!">Terms</a>
+                        <span class="mx-1">&middot;</span>
+                        <a class="small" href="#!">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
